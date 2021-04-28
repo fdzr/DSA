b3b34a65c236c02fb425e56743f86c88bf3db5b7
fdzr <fzamora.reina@gmail.com>
2021-04-28 18:23:33 -0400

    adding functionalities to delete a node in a binary tree

--- binaryTrees/deleteNode.cpp
+++ binaryTrees/deleteNode.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 
 using namespace std;
 
@@ -14,6 +15,99 @@ struct Node* newNode(int key){
     return tmp;
 }
 
+
+void inorder(Node* root){
+    if(!root)
+        return;
+
+    inorder(root->left);
+    cout<<root->key<<" ";
+    inorder(root->right);
+}
+
+void deleteDeepestNode(Node* root, Node* delete_node){
+    queue<Node*> q;
+    Node* tmp;
+
+    q.push(root);
+
+    while(!q.empty()){
+        tmp = q.front();
+        q.pop();
+
+        if(tmp == delete_node){
+            tmp = nullptr;
+            delete delete_node;
+            return;
+        }
+        
+        if(tmp->right){
+            if(tmp->right == delete_node){
+                tmp->right = nullptr;
+                delete delete_node;
+                return;
+            }
+            else
+                q.push(tmp->right);
+
+        }
+        if(tmp->left){
+            if(tmp->left == delete_node){
+                tmp->left = nullptr;
+                delete delete_node;
+                return;
+            }
+            else
+                q.push(tmp->left);
+        }
+    }
+}
+
+
+Node* deletion(Node* root, int key){
+    queue<Node*> q;
+    Node* tmp = nullptr;
+    Node* key_node;
+
+    q.push(root);
+
+    while(!q.empty()){
+        tmp = q.front();
+        q.pop();
+        
+        if(tmp->key == key)
+            key_node = tmp;
+        if(tmp->left)
+            q.push(tmp->left);
+        if(tmp->right)
+            q.push(tmp->right);
+    }
+
+    if(key_node != nullptr){
+        int value = tmp->key;
+        deleteDeepestNode(root, tmp);
+        key_node->key = value;
+    }
+    return root;
+}
+
+
 int main(){
+    Node* root = newNode(10);
+    root->left = newNode(11);
+    root->left->left = newNode(7);
+    root->left->right = newNode(12);
+    root->right = newNode(9);
+    root->right->left = newNode(15);
+    root->right->right = newNode(8);
+
+    inorder(root);
+
+    int key = 11;
+    root = deletion(root, key);
+
+    cout<<endl;
+    inorder(root);
+
     return 0;
 }
