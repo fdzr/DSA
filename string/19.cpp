e5722451ab0ca56b24e8704632e0592d1665056b
fdzr <fzamora.reina@gmail.com>
2022-12-16 14:59:13 -0300

    implementation

---
+++ string/19.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+    int 
+
+}
+
+int main() {
+    vector<string> garbage = {"G","P","GP","GG"};
+    vector<int> travel = {2, 4, 3};
+    
+    cout<<garbageCollection(garbage, travel);
+    return 0;
+}
