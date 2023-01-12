3baf8e7d76aa999e8de64cc9df100d2d45f9dfb7
fdzr <fzamora.reina@gmail.com>
2023-01-12 15:49:14 -0300

    incomplete exercise

--- binaryTrees/28.cpp
+++ binaryTrees/28.cpp
@@ -1,3 +1,28 @@
 //
 // Created by fzamora on 12-01-23.
 //
+#include <bits/stdc++.h>
+#include "../common/tree.h"
+
+using namespace std;
+using namespace Tree;
+
+string tree2str(TreeNode* root) {
+    if(!root)
+        return "";
+
+    return to_string(root->val) + "(" + tree2str(root->left) + ")" + "(" + tree2str(root->right) + ")";
+}
+
+int main() {
+    TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->left->right = createNode(4);
+    root->right = createNode(3);
+
+    cout<<tree2str(root);
+
+    return 0;
+}
+
+
