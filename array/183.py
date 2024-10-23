21b4f32fc24db10f6e09fe27c690a05581d539b4
fdzr <fzamora.reina@gmail.com>
2024-10-23 12:12:59 -0300

    exercise

---
+++ array/183.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def fairCandySwap(aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+    a = set(aliceSizes)
+    b = set(bobSizes)
+    alice_sum = sum(aliceSizes)
+    bob_sum = sum(bobSizes)
+
+    half = (alice_sum + bob_sum) // 2
+    diff = abs(alice_sum - half)
+
+    if alice_sum < half:
+        for elem in aliceSizes:
+            if elem + diff in b:
+                return [elem, elem + diff]
+    else:
+        for elem in bobSizes:
+            if elem + diff in a:
+                return [elem + diff, elem]
+
+
+print(fairCandySwap([2], [1, 3]))
