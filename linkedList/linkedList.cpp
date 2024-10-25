a7a815e096086255ad75b363ca30f0631d4dab87
fdzr <fzamora.reina@gmail.com>
2024-10-25 10:13:57 -0300

    linkedlist [implementing insert at the end]

--- linkedList/linkedList.cpp
+++ linkedList/linkedList.cpp
@@ -9,6 +9,11 @@ int main() {
     for(int i = 0; i < 10; ++i)
         l.insertAtFront(i);
 
+    for(int i = 1; i < 10; ++i)
+        l.insertAtEnd(i);
+
+    l.insertAtFront(10);
+
     l.print();
 
 }
