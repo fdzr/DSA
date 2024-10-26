bd093f0aa7392f63b88e43dd652cca9ca2f2498c
fdzr <fzamora.reina@gmail.com>
2024-10-26 13:31:06 -0300

    refactoring

---
+++ linkedList/linkedList/linkedList.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include "linkedList.hpp"
+
+using namespace std;
+
+int main() {
+    LinkedList l;
+    
+    for(int i = 1; i <= 10; ++i)
+        l.insertAtFront(i);
+
+    Node* result = l.findMiddleNode();
+    cout<<result->value<<"\n";
+
+    l.print();
+
+    l.reverse();
+    
+    l.print();
+    l.insertAtEnd(11);
+    l.print();
+    l.insertAtFront(-1);
+    l.print();
+    l.reverse();
+    l.print();
+
+ 
+
+    auto const&[found, node] = l.search(14);
+    cout<<boolalpha<<found<<"\n";
+
+    auto p = l.search(10);
+    cout<<p.second->value<<"\n";
+
+    bool removed = l.removeKey(10);
+    cout<<boolalpha<<removed<<"\n";
+    l.print();
+
+    l.removeKey(-1);
+    l.print();
+    l.removeKey(11);
+    l.print();
+    l.removeKey(15);
+    l.print();
+
+    auto temp = l.getNthNode(3);
+    cout<<temp->value<<"\n";
+
+    auto ans = l.getNthNodeFromTheEnd(5);
+    cout<<ans->value<<"\n";
+    
+    l.clear();
+    l.print();
+
+    return 0;
+    
+}
