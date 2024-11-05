c310f0807327d67b8f961904c6543ce95b7302b3
fdzr <fzamora.reina@gmail.com>
2024-11-05 15:48:49 -0300

    exercise using DFS

--- graph/problems/water-flow.cpp
+++ graph/problems/water-flow.cpp
@@ -38,6 +38,22 @@ void BFS(const Graph &G, Matrix &visited, queue<pair<int, int>> &q)
     }
 }
 
+void DFS(const Graph &G, Matrix &visited, int row, int col)
+{
+    visited[row][col] = 1;
+
+    for (int i = 0; i < directionRows.size(); ++i)
+    {
+        int newRow = row + directionRows[i];
+        int newCol = col + directionCols[i];
+
+        if (isValid(G, visited, newRow, newCol) && G[row][col] <= G[newRow][newCol])
+        {
+            DFS(G, visited, newRow, newCol);
+        }
+    }
+}
+
 int BFSDisconnected(const Graph &G)
 {
     Matrix visitedP(G.size(), vector<int>(G[0].size(), 0));
@@ -74,18 +90,61 @@ int BFSDisconnected(const Graph &G)
     return answer;
 }
 
+int DFSDisconnected(Graph &G)
+{
+    Matrix visitedP(G.size(), vector<int>(G[0].size(), 0));
+    Matrix visitedA(G.size(), vector<int>(G[0].size(), 0));
+
+    for (int i = 0; i < G[0].size(); ++i)
+    {
+        DFS(G, visitedP, 0, i);
+        DFS(G, visitedA, G.size() - 1, i);
+    }
+
+    for (int j = 0; j < G.size(); ++j)
+    {
+        DFS(G, visitedP, j, 0);
+        DFS(G, visitedA, j, G[0].size() - 1);
+    }
+
+    int answer = 0;
+    for (int i = 0; i < G.size(); ++i)
+    {
+        for (int j = 0; j < G[0].size(); ++j)
+        {
+            if (visitedA[i][j] == visitedP[i][j])
+                ++answer;
+        }
+    }
+
+    return answer;
+}
+
 int main()
 {
-    Graph G(2, vector<int>(2, 0));
+    Graph G(5, vector<int>(5, 0));
+    
+    G[0] = {1, 2, 2, 3, 5};
+    G[1] = {3, 2, 3, 4, 4};
+    G[2] = {2, 4, 5, 3, 1};
+    G[3] = {6, 7, 1, 4, 5};
+    G[4] = {5, 1, 1, 2, 4};
+
+    printf("%d\n", BFSDisconnected(G));
+    printf("%d\n", DFSDisconnected(G));
+
+    printf("\n");
+
+    G.clear();
+    G.resize(2);
+
     G[0] = {2, 2};
     G[1] = {2, 2};
-    // G[0] = {1, 2, 2, 3, 5};
-    // G[1] = {3, 2, 3, 4, 4};
-    // G[2] = {2, 4, 5, 3, 1};
-    // G[3] = {6, 7, 1, 4, 5};
-    // G[4] = {5, 1, 1, 2, 4};
 
-    printf("%d", BFSDisconnected(G));
+    printf("%d\n", BFSDisconnected(G));
+    printf("%d\n", DFSDisconnected(G));
+
+
 
     return 0;
 }
