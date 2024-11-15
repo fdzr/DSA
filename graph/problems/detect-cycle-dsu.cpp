57cd794f3150f573457abd94734326f7568b48ad
fdzr <fzamora.reina@gmail.com>
2024-11-15 15:48:19 -0300

    detecting cycles in a undirected graph using disjoint set data structure

--- graph/problems/detect-cycle-dsu.cpp
+++ graph/problems/detect-cycle-dsu.cpp
@@ -16,8 +16,14 @@ struct DSU
         for (int i = 0; i < V; ++i)
         {
             parent[i] = -1;
-            rank[i] = 1;
+            rank[i] = 0;
         }
+    }
+
+    ~DSU()
+    {
+        delete rank;
+        delete parent;
     }
 
     int find(int u)
@@ -35,15 +41,9 @@ struct DSU
         if (x != y)
         {
             if (rank[x] < rank[y])
-            {
                 parent[x] = y;
-                rank[y] += 1;
-            }
             else if (rank[x] > rank[y])
-            {
                 parent[y] = x;
-                rank[x] += 1;
-            }
             else
             {
                 parent[y] = x;
@@ -53,18 +53,35 @@ struct DSU
     }
 };
 
-bool detectCycle(Graph G, int source)
+struct hashType
 {
-    queue<int> q;
-    q.push(source);
-    vector<int> visited(8, 0);
-    visited[source] = 1;
+    int operator()(const pair<int, int>& T) const
+    {
+        return hash<int>()(T.first) ^ (hash<int>()(T.second) << 1);
+    }
+};
 
+bool detectCycle(const Graph &G)
+{
     DSU ds(G.size());
+    unordered_set<pair<int, int>, hashType> visited;
 
-    for(int u = 0; G.size(); ++u)
+    for (int u = 0; u < G.size(); ++u)
+    {
+        for (const auto &edge : G[u])
+        {
+            int x = ds.find(u);
+            int y = ds.find(edge);
 
-    return true;
+            if (visited.find({u, edge}) == visited.end() && visited.find({edge, u}) == visited.end() && x == y)
+                return true;
+
+            ds.unite(x, y);
+            visited.insert({u, edge});
+        }
+    }
+
+    return false;
 }
 
 void addEdge(Graph &G, int u, int v)
@@ -75,17 +92,26 @@ void addEdge(Graph &G, int u, int v)
 
 int main()
 {
-    Graph G(8);
-    addEdge(G, 0, 1);
-    addEdge(G, 1, 2);
-    addEdge(G, 1, 4);
-    addEdge(G, 2, 3);
-    addEdge(G, 4, 5);
-    addEdge(G, 5, 6);
-    // addEdge(G, 6, 1);
-    addEdge(G, 5, 7);
+    Graph G(6);
+    // addEdge(G, 0, 1);
+    // addEdge(G, 1, 2);
+    // addEdge(G, 1, 4);
+    // addEdge(G, 2, 3);
+    // addEdge(G, 4, 5);
+    // addEdge(G, 5, 6);
+    // // addEdge(G, 6, 1);
+    // addEdge(G, 5, 7);
 
-    cout << boolalpha << detectCycle(G, 0);
+    addEdge(G, 0, 1);
+    addEdge(G, 0, 2);
+
+    addEdge(G, 2, 3);
+    addEdge(G, 2, 4);
+    addEdge(G, 2, 5);
+
+    addEdge(G, 4, 5);
+
+    cout << boolalpha << detectCycle(G);
 
     return 0;
 }
