2de928933ce3977be6ef29bc91e03f1f382380dd
fdzr <fzamora.reina@gmail.com>
2024-11-23 17:04:05 -0300

    Kahn algorithm for Topological sorting

--- graph/Kahn-algorithm/1.cpp
+++ graph/Kahn-algorithm/1.cpp
@@ -5,25 +5,36 @@ using namespace std;
 using Graph = vector<vector<int>>;
 
 void topologicalSort(const Graph &G, vector<int> &inDegree) {
-    vector<int> S;
+    queue<int> S;
     vector<int> L;
 
     for (int i = 0; i < inDegree.size(); ++i) {
         if (inDegree[i] == 0)
-            S.push_back(i);
+            S.push(i);
     }
 
     while (!S.empty()) {
-        int u = S.back();
-        S.pop_back();
+        int u = S.front();
+        S.pop();
+
+        L.push_back(u);
 
         for (const auto &v : G[u]) {
             --inDegree[v];
 
-            if(inDegree[v] == 0)
-                S.push_back(v);
+            if (inDegree[v] == 0)
+                S.push(v);
         }
     }
+
+    for (const auto &elem : L)
+        cout << elem << " ";
+
+    cout<<"\n";
+}
+
+bool hasCycle(const vector<int> &inDegree) {
+    return accumulate(inDegree.begin(), inDegree.end(), 0) != 0;
 }
 
 void addEdge(Graph &G, int src, int dest, vector<int> &inDegree) {
@@ -32,5 +43,26 @@ void addEdge(Graph &G, int src, int dest, vector<int> &inDegree) {
 }
 
 int main() {
+    Graph G(8);
+    vector<int> inDegree(8, 0);
+
+    addEdge(G, 0, 6, inDegree);
+
+    addEdge(G, 1, 2, inDegree);
+    addEdge(G, 1, 4, inDegree);
+    addEdge(G, 1, 6, inDegree);
+
+    addEdge(G, 3, 0, inDegree);
+    addEdge(G, 3, 4, inDegree);
+
+    addEdge(G, 5, 1, inDegree);
+
+    addEdge(G, 7, 0, inDegree);
+    addEdge(G, 7, 1, inDegree);
+
+    topologicalSort(G, inDegree);
+
+    cout << boolalpha << "has Cycle?: " << hasCycle(inDegree);
+
     return 0;
 }
