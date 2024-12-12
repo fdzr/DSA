bd2658f7ee4a9b193e626fac06c897e4c86d4489
fdzr <fzamora.reina@gmail.com>
2024-12-12 10:54:34 -0300

    exercise[optimized solution]

--- string/121.cpp
+++ string/121.cpp
@@ -18,17 +18,36 @@ void dfs(int start, string s, unordered_set<string> &uniqueSubs, int &maxQuantit
     }
 }
 
+void DFSOptimized(int start, string s, unordered_set<string> &uniqueSubs,
+                  int &maxQuantity) {
+    if (start >= s.size()) {
+        maxQuantity = max(maxQuantity, static_cast<int>(uniqueSubs.size()));
+        return;
+    }
+
+    for (int i = start; i < s.size(); ++i) {
+        string chunk = s.substr(start, i - start + 1);
+        if (uniqueSubs.insert(chunk).second) {
+            DFSOptimized(i + 1, s, uniqueSubs, maxQuantity);
+            uniqueSubs.erase(chunk);
+        } else {
+            continue;
+        }
+    }
+}
+
 int maxUniqueSplit(string s) {
     unordered_set<string> uniqueSubs;
     int maxQuantity = -1;
 
-    dfs(0, s, uniqueSubs, maxQuantity);
+    // dfs(0, s, uniqueSubs, maxQuantity);
+    DFSOptimized(0, s, uniqueSubs, maxQuantity);
 
     return maxQuantity;
 }
 
 int main() {
-    string s = "ababccc";
+    string s = "aa";
     cout << maxUniqueSplit(s);
 
     return 0;
