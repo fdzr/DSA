7aba364a69e34e8c3fd14c60827ca5c4d5a99d68
fdzr <fzamora.reina@gmail.com>
2024-12-15 16:35:55 -0300

    second version of the exercise

--- array/188.cpp
+++ array/188.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+class Item {
+   public:
+    int value;
+    int index;
+    int indexOfList;
+
+    Item() = default;
+    Item(int v, int i, int iol) : value{v}, index{i}, indexOfList{iol} {}
+
+    // bool operator<(const Item& other) const { return this->value < other.value; }
+    bool operator>(const Item& other) const { return this->value > other.value; }
+};
+
 using vvi = vector<vector<int>>;
+
+/*
+k -> average values in lists
+n - > size of list of lists
+O (k * k * n)
+
+*/
 
 vector<int> mergeKSortedList(vvi lists) {
     vector<int> answer(lists.size() * lists[0].size());
@@ -15,7 +35,7 @@ vector<int> mergeKSortedList(vvi lists) {
     for (int i = answer.size() - 1; i >= 0; --i) {
         int index = -1;
         int maxElement = INT_MIN;
-        for (int j = 0; j < indexes.size() ; ++j) {
+        for (int j = 0; j < indexes.size(); ++j) {
             if (indexes[j] >= 0 && lists[j][indexes[j]] > maxElement) {
                 maxElement = lists[j][indexes[j]];
                 index = j;
@@ -29,6 +49,46 @@ vector<int> mergeKSortedList(vvi lists) {
     return answer;
 }
 
+/*
+k -> average values in lists
+n -> size of list of lists
+O (k * n * log(k))
+
+*/
+
+vector<int> mergeKSortedList2(const vvi& lists) {
+    int k = 0;
+    int j = -1;
+    for (int i = 0; i < lists.size(); ++i) k += lists[i].size();
+
+    vector<int> answer(k, 0);
+
+    priority_queue<Item, vector<Item>, greater<Item>> pq;
+
+    for (int i = 0; i < lists.size(); ++i) {
+        pq.push(Item(lists[i][0], 0, i));
+    }
+
+    while (!pq.empty()) {
+        Item item = pq.top();
+        pq.pop();
+
+        if (item.index < lists[item.indexOfList].size()) {
+            ++j;
+            pq.push(
+                Item(
+                    lists[item.indexOfList][item.index + 1], 
+                    item.index + 1,
+                    item.indexOfList
+                )
+            );
+            answer[j] = item.value;
+        }
+    }
+
+    return answer;
+}
+
 int main() {
     vvi lists = {{10, 20, 30, 40},
                  {15, 25, 35, 45},
@@ -36,7 +96,7 @@ int main() {
                  {32, 33, 39, 50},
                  {16, 18, 22, 28}};
 
-    for (int e : mergeKSortedList(lists)) cout << e << " ";
+    for (int e : mergeKSortedList2(lists)) cout << e << " ";
 
     return 0;
 }
