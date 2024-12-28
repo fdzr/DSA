50a444e26d85bcd942f4ff09780a7fb86965eb27
fdzr <fzamora.reina@gmail.com>
2024-12-28 00:14:30 -0300

    refactoring and adding tests

--- graph/eulerian-paths-and-circuit/1.cpp
+++ graph/eulerian-paths-and-circuit/1.cpp
@@ -21,7 +21,7 @@ bool isConnected(Graph &G) {
     }
 
     if (v == G.size())
-        return true;
+        return false;
 
     vector<int> visited(G.size(), 0);
 
@@ -37,24 +37,67 @@ bool isConnected(Graph &G) {
 
 int isEulerian(Graph &G) {
     if(isConnected(G) == false)
-        return false;
+        return 0;
 
 
-    int odd = 1;
+    int odd = 0;
     for(int i = 0; i < G.size(); ++i) {
         if(G[i].size() & 1)
             ++odd;
     }
 
     if(odd > 2)
-        return false;
+        return 0;
 
     return (odd)? 1: 2;
 
 }
 
-int main() {
+void addEdge(Graph &G, int v, int w) {
+    G[v].push_back(w);
+    G[w].push_back(v);
+}
 
+void test(Graph &G) {
+    int answer = isEulerian(G);
+    if(answer ==0 )
+        cout<<"no Eulerian..."<<"\n";
+    else if(answer == 1)
+        cout<<"Eulerian Path..."<<"\n";
+    else
+        cout<<"Eulerian circuit..."<<"\n";
+}
+
+int main() {
+    Graph G(5);
+    addEdge(G, 1, 0);
+    addEdge(G, 0, 2);
+    addEdge(G, 2, 1);
+    addEdge(G, 0, 3);
+    addEdge(G, 3, 4);
+    test(G);
+
+    G.clear();
+    G.resize(5);
+
+    addEdge(G, 1, 0);
+    addEdge(G, 0, 2);
+    addEdge(G, 2, 1);
+    addEdge(G, 0, 3);
+    addEdge(G, 3, 4);
+    addEdge(G, 4, 0);
+    test(G);
+
+    G.clear();
+    G.resize(5);
+
+    addEdge(G, 1, 0);
+    addEdge(G, 0, 2);
+    addEdge(G, 2, 1);
+    addEdge(G, 0, 3);
+    addEdge(G, 3, 4);
+    addEdge(G, 1, 3);
+    test(G);
 
     return 0;
 }
