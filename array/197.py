cc9e3e814ac7a37e00e8408c185c981e8bd5f89c
fdzr <fzamora.reina@gmail.com>
2025-03-01 19:14:15 -0300

    exercise

---
+++ array/197.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def moveZeroes(nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+
+    L, R = 0, 1
+
+    while R < len(nums):
+        if nums[L] != 0:
+            L += 1
+            R = L + 1
+            continue
+        if nums[R] != 0:
+            nums[L], nums[R] = nums[R], nums[L]
+            R += 1
+            L += 1
+        else:
+            R += 1
+
+
+nums = [0, 1, 0, 3, 12]
+moveZeroes(nums)
+
+print(nums)
