67319ef27983326280361b9b48b333a28b1d6031
fdzr <fzamora.reina@gmail.com>
2025-03-05 00:04:52 -0300

    test functionalities

--- test-functionalities/main.py
+++ test-functionalities/main.py
@@ -1,28 +1,38 @@
 from collections import deque
+from typing import List
 
 
-def permutations(input):
-    perms = []
-    solution = []
+# def permutations(input):
+#     perms = []
+#     solution = []
 
-    def backtrack():
-        if len(input) == len(solution):
-            perms.append(solution[:])
-            print(perms)
-            return
+#     def backtrack():
+#         if len(input) == len(solution):
+#             perms.append(solution[:])
+#             print(perms)
+#             return
 
-        for num in input:
-            if not num in solution:
-                solution.append(num)
-                backtrack()
-                solution.pop()
-                print(num)
+#         for num in input:
+#             if not num in solution:
+#                 solution.append(num)
+#                 backtrack()
+#                 solution.pop()
+#                 print(num)
 
-    return backtrack()
+#     return backtrack()
 
 
-# permutations([1, 2, 3])
-d = deque()
-d.append(1)
-print(d.popleft())
-print(d.popleft())
+# # permutations([1, 2, 3])
+# d = deque()
+# d.append(1)
+# print(d.popleft())
+# print(d.popleft())
+
+
+def reverseList(a: List[int]):
+    b = [e for e in a if e % 2 == 0]
+    b.reverse()
+    return b
+
+
+print(reverseList([1, 2, 3, 4]))
