408a67729496c6cc7436c2992a9788e000fb0805
fdzr <fzamora.reina@gmail.com>
2025-04-08 12:29:23 -0400

    testing

--- test-functionalities/main.py
+++ test-functionalities/main.py
@@ -34,3 +34,38 @@ def reverseList(a: List[int]):
 
 
 print(reverseList([1, 2, 3, 4]))
+
+
+class test:
+    id = 0
+
+    def __init__(self, id):
+        self.id = id
+        id = 2
+
+
+t = test(1)
+print(t.id)
+
+a: float = 1
+
+
+a = 7
+print(a.__str__())
+
+import re
+
+m = re.search(r"(ab[cd]?)", "acdeabdabcde")
+print(m.groups())
+
+
+import itertools
+
+print()
+print(list(itertools.islice(itertools.count(5), 10)))
+
+
+text = "abc123def456ghi"
+patter = r"(?:abc)\d+"
+matches = re.findall(patter, text)
+print(matches)
