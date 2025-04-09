801ef57a1d0e46d0049defdba19772f70576fc89
fdzr <fzamora.reina@gmail.com>
2025-04-08 21:07:56 -0400

    implementing a function to print all ancestors of a given node in a binary tree

--- binaryTrees/problems/find-ancestor-of-a-given-node.py
+++ binaryTrees/problems/find-ancestor-of-a-given-node.py
@@ -9,21 +9,33 @@ class Node:
 
 
 def find_ancestor(root: Node, node: int):
-    pass
+    if root is None:
+        return False
+
+    if root.key == node:
+        return True
+
+    ans_l = find_ancestor(root.left, node)
+    ans_r = find_ancestor(root.right, node)
+
+    if ans_l or ans_r:
+        print(root.key)
+
+    return ans_l or ans_r
 
 
 if __name__ == "__main__":
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
+    root = Node(15)
+    root.left = Node(10)
+    root.right = Node(20)
 
-    root.left.left = Node(4)
-    root.left.right = Node(5)
+    root.left.left = Node(8)
+    root.left.right = Node(12)
 
-    root.right.left = Node(6)
-    root.right.right = Node(7)
+    root.right.left = Node(16)
+    root.right.right = Node(25)
 
-    root.right.left.left = Node(8)
-    root.right.right.right = Node(9)
+    root.right.left.left = Node(18)
+    # root.right.right.right = Node(9)
 
-    find_ancestor(root, 9)
+    find_ancestor(root, 18)
