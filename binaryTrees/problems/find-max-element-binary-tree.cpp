1aaaea79d4e98ae0a24b2445532c4a19233b8331
fdzr <fzamora.reina@gmail.com>
2025-04-10 12:07:31 -0400

    exercise

---
+++ binaryTrees/problems/find-max-element-binary-tree.cpp
@@ -0,0 +1,37 @@
+#include "../../common/tree.h"
+
+using namespace Tree;
+
+int findMaxValue(TreeNode *root) {
+    if (root == nullptr) {
+        return 0;
+    }
+
+    int solLeft = findMaxValue(root->left);
+    int solRight = findMaxValue(root->right);
+
+    int currentValue = root->val;
+    int maxValue = (solLeft > solRight) ? solLeft : solRight;
+    maxValue = (currentValue > maxValue) ? currentValue : maxValue;
+
+    return maxValue;
+}
+
+int main() {
+    TreeNode *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(7);
+
+    root->right->left = createNode(5);
+    root->right->right = createNode(6);
+
+    root->right->left->left = createNode(8);
+    root->right->left->right = createNode(9);
+
+    cout << findMaxValue(root);
+
+    return 0;
+}
