693e60972e2f83796f1fb178ab4cbf3b49add56f
fdzr <fzamora.reina@gmail.com>
2025-04-29 00:00:54 -0400

    exercise

---
+++ binaryTrees/37.cpp
@@ -0,0 +1,37 @@
+#include "../common/tree.h"
+
+using namespace Tree;
+
+int findDiameter(TreeNode* root, int& cont) {
+    if (root == nullptr) return 0;
+
+    int solLeft = findDiameter(root->left, cont);
+    int solRight = findDiameter(root->right, cont);
+
+    cont = max(cont, solLeft + solRight);
+
+    return max(solLeft, solRight) + 1;
+}
+
+int solution(TreeNode* root) {
+    int cont = 0;
+    findDiameter(root, cont);
+    return cont;
+}
+
+int main() {
+    TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->left->left = createNode(5);
+    root->left->left->right = createNode(6);
+
+    root->left->left->left->left = createNode(7);
+    root->left->left->right->left = createNode(8);
+
+    cout << solution(root);
+
+    return 0;
+}
