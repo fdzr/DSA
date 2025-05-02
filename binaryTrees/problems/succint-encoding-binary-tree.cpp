8ecd9fa6d411ad1a2f5b9d6a5cd00c25a36732df
fdzr <fzamora.reina@gmail.com>
2025-05-02 16:45:36 -0400

    implementing function to decode a binary tree previously encoded

--- binaryTrees/problems/succint-encoding-binary-tree.cpp
+++ binaryTrees/problems/succint-encoding-binary-tree.cpp
@@ -15,7 +15,22 @@ void encodeTree(TreeNode *root, string &cad, vector<int> &arr) {
     encodeTree(root->right, cad, arr);
 }
 
-int main() { 
+TreeNode *decodeTree(string &cad, vector<int> &arr, int &indexCad, int &indexVector) {
+    if (cad[indexCad] == '0') {
+        ++indexCad;
+        return nullptr;
+    }
+
+    TreeNode *root = createNode(arr[indexVector++]);
+    ++indexCad;
+
+    root->left = decodeTree(cad, arr, indexCad, indexVector);
+    root->right = decodeTree(cad, arr, indexCad, indexVector);
+
+    return root;
+}
+
+int main() {
     TreeNode *root = createNode(10);
     root->left = createNode(20);
     root->right = createNode(30);
@@ -25,8 +40,12 @@ int main() {
     vector<int> arr;
 
     encodeTree(root, cad, arr);
-    cout<<cad;
+    cout << cad << "\n";
 
+    int indexCad = 0;
+    int indexVector = 0;
+    TreeNode *newRoot = decodeTree(cad, arr, indexCad, indexVector);
+    preOrder(newRoot);
 
     return 0;
 }
