dcc7ecc64010336100e2ac7ff8ea54e1bca07991
fdzr <fzamora.reina@gmail.com>
2025-05-18 21:54:24 -0400

    incomplete exercise

---
+++ binaryTrees/36.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val: int):
+        self.val = val
+        self.left: TreeNode = None
+        self.right: TreeNode = None
+
+
+def solution() -> tuple[Optional[any], int]:
+    pass
+
+
+def longestUnivaluePath(root: Optional[TreeNode]) -> int:
+    pass
