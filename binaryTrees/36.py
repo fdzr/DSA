101777b729be7743429934ea624bfecf48c27135
fdzr <fzamora.reina@gmail.com>
2025-05-22 23:15:47 -0400

    first part of the solution

--- binaryTrees/36.py
+++ binaryTrees/36.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any
+from typing import Optional, Any, List
 
 
 class TreeNode:
@@ -8,9 +8,56 @@ class TreeNode:
         self.right: TreeNode = None
 
 
-def solution() -> tuple[Optional[Any], int]:
-    pass
+class Pack:
+    def __init__(
+        self,
+        value: Optional[int] = None,
+        cont: int = 0,
+    ):
+        self.value: Optional[int] = value
+        self.cont: int = cont
+
+
+def solution(root: TreeNode, cont: List[int]) -> tuple[Optional[Any], int]:
+    if root is None:
+        return Pack()
+
+    sol_left = solution(root.left, cont)
+    sol_right = solution(root.right, cont)
+
+    pack = Pack()
+    if sol_left.value is None and sol_right.value is None:
+        pack.value = root.val
+
+    elif sol_left.value is None and sol_right.value is not None:
+        if sol_right.value == root.val:
+            pack.cont = sol_right.cont + 1
+        cont[0] = max(cont[0], pack.cont)
+
+    elif sol_left.value is not None and sol_right.value is None:
+        if sol_left.value == root.val:
+            pack.cont = sol_left.cont + 1
+        cont[0] = max(cont[0], pack.cont)
+
+    else:
+        pack.value = root.val
+        pack.cont = 0
+
+    return pack
 
 
 def longestUnivaluePath(root: Optional[TreeNode]) -> int:
-    pass
+    cont = [0]
+    sol = solution(root, cont)
+    return cont[0]
+
+
+if __name__ == "__main__":
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(5)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(1)
+    root.right.right = TreeNode(5)
+
+    print(longestUnivaluePath(root))
