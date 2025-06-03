6f305208a8c81be58d29f31b40634853335754c8
fdzr <fzamora.reina@gmail.com>
2025-06-02 23:25:12 -0400

    add test case to debug

--- binaryTrees/36.py
+++ binaryTrees/36.py
@@ -19,6 +19,9 @@ class Pack:
         self.cont: int = cont
         self.max_path: Optional[int] = max_path
 
+    # def __repr__(self):
+    #     print(f"{self.value} - {self.cont} - {self.max_path}")
+
 
 def solution(root: TreeNode, cont: List[int]) -> tuple[Optional[Any], int]:
     if root is None:
@@ -36,48 +39,51 @@ def solution(root: TreeNode, cont: List[int]) -> tuple[Optional[Any], int]:
             pack.cont = sol_right.cont + 1
             if sol_right.max_path is not None:
                 pack.cont = 1 + sol_right.max_path
-            pack.value = root.val
-        cont[0] = max(cont[0], pack.cont)
+
+            cont[0] = max(cont[0], pack.cont)
 
     elif sol_left.value is not None and sol_right.value is None:
         if sol_left.value == root.val:
             pack.cont = sol_left.cont + 1
-            if sol_left is not None:
+            if sol_left.max_path is not None:
                 pack.cont = 1 + sol_left.max_path
-            pack.value = root.val
-        cont[0] = max(cont[0], pack.cont)
+
+            cont[0] = max(cont[0], pack.cont)
 
     else:
-        if root.val == sol_left.value and root.value == sol_right.value:
-            pack.cont = sol_left.value + sol_right.value
+        if root.val == sol_left.value and root.val == sol_right.value:
+            pack.cont = sol_left.cont + sol_right.cont
             pack.max_path = max(sol_left.cont, sol_right.cont)
+            cont[0] = max(cont[0], pack.cont)
         elif root.val == sol_left.value:
             pack.cont = sol_left.cont + 1
-            pack.value = root.val
+
+            cont[0] = max(cont[0], pack.cont)
         elif root.val == sol_right.value:
             pack.cont = sol_right.cont + 1
-            pack.value = root.val
 
-        cont[0] = max(cont[0], pack.cont)
+            cont[0] = max(cont[0], pack.cont)
 
-        pack.value = root.val
-        pack.cont = 0
+    pack.value = root.val
+    # pack.cont = 0
 
     return pack
 
 
 def longestUnivaluePath(root: Optional[TreeNode]) -> int:
     cont = [0]
-    sol = solution(root, cont)
+    solution(root, cont)
     return cont[0]
 
 
 if __name__ == "__main__":
-    root = TreeNode(5)
-    root.left = TreeNode(4)
-    root.right = TreeNode(5)
-    root.left.left = TreeNode(1)
-    root.left.right = TreeNode(1)
-    root.right.right = TreeNode(5)
+    root = TreeNode(1)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(1)
+    root.right.right = TreeNode(1)
+    root.right.right.left = TreeNode(1)
+
+    root.right.left.left = TreeNode(1)
+    root.right.left.right = TreeNode(1)
 
     print(longestUnivaluePath(root))
