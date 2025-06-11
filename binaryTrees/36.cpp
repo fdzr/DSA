6e45e71d707640200df3da6bc3a635b47ae55a55
fdzr <fzamora.reina@gmail.com>
2025-06-11 18:34:26 -0400

    exercise[refactoring]

--- binaryTrees/36.cpp
+++ binaryTrees/36.cpp
@@ -2,56 +2,25 @@
 
 using namespace Tree;
 
-pair<optional<int>, int> solution(TreeNode* root, int& cont) {
-    if (root == nullptr) {
-        return {nullopt, 0};
-    }
+int solution(TreeNode* root, int& cont) {
+    if (root == nullptr)
+        return 0;
 
-    pair<optional<int>, int> solLeft = solution(root->left, cont);
-    pair<optional<int>, int> solRight = solution(root->right, cont);
+    int solLeft = solution(root->left, cont);
+    int solRight = solution(root->right, cont);
 
-    pair<optional<int>, int> solution;
+    int tempSolLeft = 0;
+    if (root->left && root->left->val == root->val)
+        tempSolLeft = solLeft + 1;
+    
+    int tempSolRight = 0;
+    if (root->right && root->right->val == root->val)
+        tempSolRight = solRight + 1;
+    
+    cont = max(cont, tempSolLeft + tempSolRight);
 
-    if (solLeft.first == nullopt && solRight.first == nullopt) {
-        solution.first = root->val;
-        solution.second = 0;
-        cont = max(cont, solution.second);
-    } else if (solLeft.first == nullopt || solRight.first == nullopt) {
-        if (solLeft.first != nullopt) {
-            int newCont = solLeft.second + (solLeft.first == root->val);
-            solution.first = root->val;
-            solution.second = newCont;
-            cont = max(cont, newCont);
-        } else {
-            int newCont = solRight.second + (solRight.first == root->val);
-            solution.first = root->val;
-            solution.second = newCont;
-            cont = max(cont, newCont);
-        }
-    } else {
-        if(solLeft.first != root->val && solRight.first != root->val) {
-            solution.first = root->val;
-            solution.second = 0;
-        } else if (solLeft.first != root->val || solRight.first != root->val) {
-            if(solLeft.first == root->val) {
-                int newCont = solLeft.second + 1;
-                solution.first = root->val;
-                solution.second = newCont;
-            } else {
-                int newCont = solRight.second + 1;
-                solution.first = root->val;
-                solution.second = newCont;
-            }
+    return max(tempSolLeft, tempSolRight);
 
-            cont = max(cont, solution.second);
-        } else {
-            solution.first = root->val;
-            solution.second = solLeft.second + solRight.second + 2;
-            cont = max(cont, solution.second);
-        }
-    }
-
-    return solution;
 }
 
 int longestUnivaluePath(TreeNode* root) {
@@ -61,14 +30,14 @@ int longestUnivaluePath(TreeNode* root) {
 }
 
 int main() {
-    TreeNode *root = createNode(5);
+    TreeNode* root = createNode(5);
     root->left = createNode(4);
     root->right = createNode(5);
     root->left->left = createNode(1);
     root->left->right = createNode(1);
     root->right->right = createNode(1);
 
-    cout<< longestUnivaluePath(root);
+    cout << longestUnivaluePath(root);
 
     return 0;
 }
