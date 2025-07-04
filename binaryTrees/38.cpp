9c9f999761525302a8e29e58d80d89aae8998e60
fdzr <fzamora.reina@gmail.com>
2025-07-04 00:59:38 -0400

    exercise[incomplete]

---
+++ binaryTrees/38.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "../common/tree.h"
+
+using namespace std;
+using namespace Tree;
+
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+
+TreeNode<int>* bstToGst(TreeNode<int>* root) {
+        
+}
+
+int main() {
+
+    return 0;
+}
