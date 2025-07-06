66767bafdcfe6f713cb1bca0f6891bed439830e3
fdzr <fzamora.reina@gmail.com>
2025-07-06 14:04:15 -0400

    exercise[incomplete]

---
+++ binaryTrees/38.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val: int):
+        self.val: int = val
+        self.left: TreeNode = None
+        self.right: TreeNode = None
+
+
+def convertBST(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    pass
