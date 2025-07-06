f81628d196e9302d76d821e85999a6eb6016deca
fdzr <fzamora.reina@gmail.com>
2025-07-06 15:50:48 -0400

    exercise

--- binaryTrees/38.cpp
+++ binaryTrees/38.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 #include "../common/tree.h"
 
 using namespace std;
@@ -6,11 +7,37 @@ using namespace Tree;
 
 // https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
 
+int convert(TreeNode<int>* root, int value) {
+    if (root == nullptr) {
+        return value;
+    }
+
+    int rightValue = convert(root->right, value);
+    root->val += rightValue;
+    int leftValue = convert(root->left, root->val);
+
+    return leftValue;
+}
+
 TreeNode<int>* bstToGst(TreeNode<int>* root) {
-        
+    convert(root, 0);
+    return root;
 }
 
 int main() {
+    TreeNode<int>* root = createNode(4);
+    root->left = createNode(1);
+    root->right = createNode(6);
+    root->left->left = createNode(0);
+    root->left->right = createNode(2);
+    root->left->right->right = createNode(3);
+    root->right->left = createNode(5);
+    root->right->right = createNode(7);
+    root->right->right->right = createNode(8);
+
+    bstToGst(root);
+
+    inOrder(root);
 
     return 0;
 }
