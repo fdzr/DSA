12ba4d5000e32932c6f036cc5de7bc3e01bd651a
fdzr <fzamora.reina@gmail.com>
2025-07-13 01:11:54 -0400

    exercise[incomplete]

---
+++ binaryTrees/40.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, k: int):
+        self.val: int = k
+        self.left: TreeNode = None
+        self.right: TreeNode = None
+
+
+def find_index_right_subtree(preorder: List[int], l: int, r: int):
+    idx = -1
+
+    for index in range(l, r + 1):
+        if preorder[index] > preorder[l]:
+            idx = index
+            break
+
+    return idx
+
+
+def insert(preorder: List[int], l: int, r: int) -> Optional[TreeNode]:
+    if l > r:
+        return None
+
+    index = find_index_right_subtree(preorder, l, r)
+    node = TreeNode(preorder[l])
+
+    if l == r or index == -1:
+        return node
+
+    node.left = insert(preorder, l + 1, index - 1)
+    node.right = insert(preorder, index, r)
+
+    return node
+
+
+def in_order(root: Optional[TreeNode]):
+    if root is None:
+        return
+
+    in_order(root.left)
+    print(root.val, end=" ")
+    in_order(root.right)
+
+
+def bstFromPreorder(preorder: List[int]) -> Optional[TreeNode]:
+    root = insert(preorder, 0, len(preorder) - 1)
+    in_order(root)
+
+
+if __name__ == "__main__":
+    preorder = [8, 5, 1, 7, 10, 12]
+    bstFromPreorder(preorder)
