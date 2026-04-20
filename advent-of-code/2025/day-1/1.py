829b1b47011bc56302b07cb5d2f78b2869d7be08
fdzr <fzamora.reina@gmail.com>
2026-04-19 23:46:50 -0400

    exercise

--- advent-of-code/2025/day-1/1.py
+++ advent-of-code/2025/day-1/1.py
@@ -13,3 +13,36 @@ with open("input.txt", "r") as f_in:
 
 
 print(cont)
+
+
+cont_2part = 0
+start = 50
+cont = 0
+
+with open("input.txt", "r") as f_in:
+    for dial in f_in.read().split():
+        if dial[0] == "R":
+            cont_2part = int(dial[1:]) // 100
+            start = start + (int(dial[1:]) % 100)
+
+            if start > 100:
+                cont += cont_2part + 1
+
+            start %= 100
+            if start == 0:
+                cont += 1
+
+        else:
+            reminder = 100 - start
+            if int(dial[1:]) - reminder > 0:
+                cont += 1
+                new_dial = int(dial[1:]) - reminder
+                cont_2part = new_dial // 100
+                cont += cont_2part
+                start = 100 - (new_dial % 100)
+            else:
+                start = (start - int(dial[1:]) + 100) % 100
+                if start == 0:
+                    cont += 1
+
+    print(cont)
