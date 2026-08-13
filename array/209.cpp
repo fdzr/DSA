c3b29cf92c9b87e5b4d511b7f090372587b6c187
fdzr <fzamora.reina@gmail.com>
2026-08-13 15:56:52 -0400

    restoring backtrack ocndition

--- array/209.cpp
+++ array/209.cpp
@@ -31,7 +31,7 @@ bool dfs(const MATRIX<char>& m, MATRIX<bool>& visited, int row, int col, string 
             }
 
             visited[newRow][newCol] = false;
-            
+            --index;
 
         }
     }
